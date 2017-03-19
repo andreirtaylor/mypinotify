fd9ce667f1b22fbdeff436aa83fdd44c0fc2fdd5
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-03-18 19:42:18 -0700

    Fixed server and split api over

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,10 @@
-var async = require('async');
+var express = require('express')
+var app = express()
 
-// Require the driver.
-var pg = require('pg');
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-// Connect to the "bank" database.
-var config = {
-  host: 'db1.mypinotify.me',
-  database: 'mypinotify',
-  user: 'user1',
-  port: 26257
-};
-
-//this initializes a connection pool
-//it will keep idle connections open for a 30 seconds
-//and set a limit of maximum 10 idle clients
-var pool = new pg.Pool(config);
-
-// to run a query we can acquire a client from the pool,
-// run a query on the client, and then return the client to the pool
-pool.connect(function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT * FROM events', function(err, result) {
-    //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
-    done(err);
-
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows);
-    //output: 1
-  });
-});
-
-pool.on('error', function (err, client) {
-  // if an error is encountered by a client while it sits idle in the pool
-  // the pool itself will emit an error event with both the error and
-  // the client which emitted the original error
-  // this is a rare occurrence but can happen if there is a network partition
-  // between your application and the database, the database restarts, etc.
-  // and so you might want to handle it and at least log it out
-  console.error('idle client error', err.message, err.stack)
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 })
