505b3daf531f9f19561b8a09c140b4e4424b406c
Andrei Taylor <andreirtaylor@gmail.com>
2017-03-18 22:14:35 -0700

    Add appid and app secred hard coded yolo bro

--- app/src/config.js
+++ app/src/config.js
@@ -29,8 +29,8 @@ export const auth = {
 
   // https://developers.facebook.com/
   facebook: {
-    id: process.env.FACEBOOK_APP_ID || '186244551745631',
-    secret: process.env.FACEBOOK_APP_SECRET || 'a970ae3240ab4b9b8aae0f9f0661c6fc',
+    id: process.env.FACEBOOK_APP_ID || '1254362078010282',
+    secret: process.env.FACEBOOK_APP_SECRET || 'a9981c1eb18a262240ecdd4e33429ac3',
   },
 
   // https://cloud.google.com/console/project
